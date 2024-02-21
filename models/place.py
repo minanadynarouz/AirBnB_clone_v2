75d36d1b3b6725c4c5e976b655686d5830623273
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-20 21:55:03 -0800

    a

--- models/place.py
+++ models/place.py
@@ -3,26 +3,10 @@
 import models
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, Float, Integer, String
-from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
     """Represents a Place for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table places.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store places.
-        city_id (sqlalchemy String): The place's city id.
-        user_id (sqlalchemy String): The place's user id.
-        name (sqlalchemy String): The name.
-        description (sqlalchemy String): The description.
-        number_rooms (sqlalchemy Integer): The number of rooms.
-        number_bathrooms (sqlalchemy Integer): The number of bathrooms.
-        max_guest (sqlalchemy Integer): The maximum number of guests.
-        price_by_night (sqlalchemy Integer): The price by night.
-        latitude (sqlalchemy Float): The place's latitude.
-        longitude (sqlalchemy Float): The place's longitude.
     """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
