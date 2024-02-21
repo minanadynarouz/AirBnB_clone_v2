0af6a40a0d5f304bcca954aaa19bff2634ea9354
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-20 21:41:23 -0800

    Task 8

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Defines the User class."""
+from hashlib import md5
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -15,9 +17,20 @@ class User(BaseModel, Base):
         password (sqlalchemy String): The user's password.
         first_name (sqlalchemy String): The user's first name.
         last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The User-Place relationship.
+        reviews (sqlalchemy relationship): The User-Review relationship.
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
+
+    def __setattr__(self, name, value):
+        """Encodes passwords using MD5 before setting an attribute."""
+        if name == "password":
+            value = value.encode("utf-8")
+            value = md5(value).hexdigest()
+        object.__setattr__(self, name, value)
