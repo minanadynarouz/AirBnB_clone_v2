e303b974b0e43b0b5a962ae6537b7479d1bfb27f
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-20 22:13:31 -0800

    a

--- models/user.py
+++ models/user.py
@@ -1,36 +1,18 @@
-#!/usr/bin/env python3
-"""Defines the User class."""
-from hashlib import md5
-from models.base_model import Base, BaseModel
+#!/usr/bin/python3
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Represents a user for a MySQL database.
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
 
-    Inherits from SQLAlchemy Base and links to the MySQL table users.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
-    """
-    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    def __setattr__(self, name, value):
-        """Encodes passwords using MD5 before setting an attribute."""
-        if name == "password":
-            value = value.encode("utf-8")
-            value = md5(value).hexdigest()
-        object.__setattr__(self, name, value)
+    places = relationship('Place', backref='user', cascade='all, delete')
+    reviews = relationship('Review', backref='user', cascade='all, delete')
