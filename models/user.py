e526ee03ebf6fc16b559b32866fe2f4c0fb42c24
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-20 22:00:56 -0800

    user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Defines the User class."""
-from models.base_model import Base, BaseModel
+import models
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
