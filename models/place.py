5867a668a931a6794fc172382a7849d1077afb18
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-20 22:00:52 -0800

    place

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Defines the Place class."""
 import models
-from models.base_model import Base, BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Float, Integer, String
 
 
@@ -12,10 +12,10 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
