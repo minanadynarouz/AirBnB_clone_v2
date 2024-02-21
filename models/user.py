ebb52bff59c70192d80a0e91a9a434a371d3bd62
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-20 21:35:42 -0800

    l

--- models/user.py
+++ models/user.py
@@ -1,13 +1,21 @@
-#!/usr/bin/python3
-""" holds class User"""
-
-from models.base_model import BaseModel, Base
+#!/usr/bin/env python3
+"""Defines the User class."""
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """Represents a user for a MySQL database.
 
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
