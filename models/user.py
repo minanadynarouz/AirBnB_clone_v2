a8f64ef3af5de6d5c97f902d24234dad8ded444c
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-20 21:27:52 -0800

    a

--- models/user.py
+++ models/user.py
@@ -1,25 +1,37 @@
-#!/usr/bin/env python3
-"""Defines the User class."""
-from hashlib import md5
-from models.base_model import Base, BaseModel
+#!/usr/bin/python3
+""" holds class User"""
+
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
-    """Represents a user for a MySQL database.
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
 
     def __setattr__(self, name, value):
-        """Encodes passwords using MD5 before setting an attribute."""
+        """sets a password with md5 encryption"""
         if name == "password":
-            value = value.encode("utf-8")
-            value = md5(value).hexdigest()
-        object.__setattr__(self, name, value)
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
