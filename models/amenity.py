9b94a238ab5fb64fb9c6336816a6c7d5a8c13966
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-21 20:13:56 -0800

    last task

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from models.place import place
 
 class Amenity(BaseModel, Base):
-    '''The class for Amenity'''
-    __tablename__ = 'amenities'
+    """The class for Amenity"""
 
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", backref="Amenity")
