5f0b526033c23fd9fb0cc4094880873266df356a
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-21 20:15:11 -0800

    a

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from models.place import place
 
 class Amenity(BaseModel, Base):
     """The class for Amenity"""
 
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", backref="Amenity")
