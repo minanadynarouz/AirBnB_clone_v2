90dda199d413b4ee5191a3381daca6d8440a3968
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-12 13:56:51 -0700

    amend

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 # sets up the web servers for the deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo '
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+' > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -hR ubuntu:ubuntu /data/
+
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+
+sudo service nginx restart
