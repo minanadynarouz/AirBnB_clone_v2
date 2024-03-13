c0dbfe0bbd95bcf7d6111e05dfe6c3a8339f2ba9
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-13 16:53:40 -0700

    amend

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,6 @@
 # sets up the web servers for the deployment of web_static
 
 sudo apt-get update
-sudo apt-get upgrade
 sudo apt-get -y install nginx
 
 mkdir -p /data/web_static/releases/test/
@@ -22,6 +21,6 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -hR ubuntu:ubuntu /data/
 
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
