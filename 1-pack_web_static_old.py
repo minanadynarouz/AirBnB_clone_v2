2d661159d83577b3a06afb24716ec5aef98a4b06
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-15 23:59:28 -0700

    Amendment 2

--- 1-pack_web_static_old.py
+++ 1-pack_web_static_old.py
@@ -4,9 +4,10 @@ Fabric script that generates a tgz archive from the contents of the web_static
 folder of the AirBnB Clone repo
 """
 
-from fabric.api import *
-from os.path import isdir
 from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
 
 def do_pack():
     """generates a tgz archive"""
@@ -14,7 +15,7 @@ def do_pack():
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
             local("mkdir versions")
-        file_name = "versions/web_static_{}".format(date)
+        file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
     except:
--- 2-do_deploy_web_static_old.py
+++ 2-do_deploy_web_static_old.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.user = 'ubuntu'
+
 env.hosts = ['18.233.63.75', '54.160.98.1']
 
 def do_deploy(archive_path):
