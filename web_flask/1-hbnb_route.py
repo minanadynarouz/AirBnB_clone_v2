5b55173ceae0d18033dd99755d5dd719bc139833
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-22 09:17:15 -0700

    amend

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,24 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
 from flask import Flask
-"""starts a Flask web application"""
 
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def display_hello():
-    """Display Hello HBNB"""
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Display HBNB"""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
